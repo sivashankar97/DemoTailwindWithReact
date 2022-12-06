8825d7bb16abc50e76dcc04f08027212b7d1cb14
sivashankar97 <hexshankar1997@gmail.com>
2022-12-06 16:04:24 +0530

    feat(demo): tailwinCss configure to demo-tailwincss project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+   <section className="bg-gray-50">
+  <div
+    className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
+  >
+    <div className="mx-auto max-w-xl text-center">
+ 
+
+    
+
+      <div className="mt-8 flex flex-wrap justify-center gap-8">
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+          href="/get-started"
         >
-          Learn React
+          Get Started
         </a>
-      </header>
+
+        <a
+          className="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+          href="/about"
+        >
+          Learn More
+        </a>
+    <a
+  className="inline-flex items-center rounded border border-indigo-600 bg-indigo-600 px-8 py-3 text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+  href="/download"
+>
+  <span className="text-sm font-medium"> Download </span>
+
+  <svg
+    className="ml-3 h-5 w-5"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M17 8l4 4m0 0l-4 4m4-4H3"
+    />
+  </svg>
+</a>
+
+<a
+  className="inline-flex items-center rounded border border-indigo-600 px-8 py-3 text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
+  href="/download"
+>
+  <span className="text-sm font-medium"> Download </span>
+
+  <svg
+    className="ml-3 h-5 w-5"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M17 8l4 4m0 0l-4 4m4-4H3"
+    />
+  </svg>
+</a>
+
+      </div>
     </div>
-  );
+  </div>
+
+
+
+
+</section>
+
+
+
+  );  
 }
 
 export default App;
