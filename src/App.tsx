08407eded8c17bc68431b3859d49eaa8bd687d58
sivashankar97 <hexshankar1997@gmail.com>
2022-12-07 19:41:25 +0530

    feat(demo): add custom color config the tailwindconfig file

--- src/App.tsx
+++ src/App.tsx
@@ -51,61 +51,88 @@ function App() {
   </svg>
 </a>
 
+<a
+  className="inline-flex items-center rounded border border-indigo-600 px-8 py-3 text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
+  href="/download"
+>
+  <span className="text-sm font-medium"> Download </span>
 
+  <svg
+    className="ml-3 h-5 w-5"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M17 8l4 4m0 0l-4 4m4-4H3"
+    />
+  </svg>
+</a>
 
 <hr />
       <Button 
-      onClick={()=> alert('Button deafualt is clicked !')}
+      onClick={()=> alert('Button 1 is clicked !')}
       variant="default"
       size="sm"
       > Default </Button>
 
       <Button 
-      onClick={()=> alert('Button primary is clicked !')}
+      onClick={()=> alert('Button 1 is clicked !')}
       variant="primary"
       size="sm"
       > Primary </Button>
 
       <Button 
-      onClick={()=> alert('Button info is clicked !')}
+      onClick={()=> alert('Button 1 is clicked !')}
       variant="info"
       size="sm"
       > Info </Button>
 
       <Button 
-      onClick={()=> alert('Button Warning is clicked !')}
+      onClick={()=> alert('Button 1 is clicked !')}
       variant="warning"
       size="sm"
       > Warning </Button>
 
       <Button 
-      onClick={()=> alert('Button Success is clicked !')}
+      onClick={()=> alert('Button 1 is clicked !')}
       variant="success"
       size="sm"
       > Success </Button>
 
       <Button 
-      onClick={()=> alert('Button Danger is clicked !')}
-      variant="danger"
+      onClick={()=> alert('Button 1 is clicked !')}
+      variant="bg-secondary"
       size="sm"
       > Danger </Button>
 
       <Button 
-      onClick={()=> alert('Button Dark is clicked !')}
-      variant="dark"
+      onClick={()=> alert('Button 1 is clicked !')}
+      variant="bg-primary"
       size="sm"
       > Dark </Button>
 
       <Button 
-      onClick={()=> alert('Button Deafult is clicked !')}
+      onClick={()=> alert('Button 1 is clicked !')}
       variant="default"
       size="sm"
       disabled={true}
       > Disabled </Button>
 
+      <hr />
+
     </div>
       </div>
     </div>
+
+
+
+  
+
 </section>
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#1fb6ff",
+      secondary: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
   },
   plugins: [],
 };
